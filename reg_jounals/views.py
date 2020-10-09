01fdc5ad35bcb0f69c808ed5378c2dd36c5695e9
Jason8558 <71411694+Jason8558@users.noreply.github.com>
2020-10-09 16:50:24 +1200

    Добавлена проверка на авторизацию

--- reg_jounals/views.py
+++ reg_jounals/views.py
@@ -6,6 +6,11 @@ from .models import *
 def index(request):
     return render(request, 'reg_jounals/index.html')
 def outbound_docs(request):
-    documents = OutBoundDocument.objects.all()
-    count = len(documents)
-    return render(request, 'reg_jounals/outbound_docs.html', context={'documents':documents, 'count':count})
+    if request.user.is_authenticated:
+        documents = OutBoundDocument.objects.all()
+        count = len(documents)
+        method = str(request.method)
+        usr = str(request.user.first_name)
+        return render(request, 'reg_jounals/outbound_docs.html', context={'documents':documents, 'count':count})
+    else:
+        return HttpResponse('Неавторизованный персонал!!!')
