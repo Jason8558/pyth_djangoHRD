413b66819f7400011afc6324d3637760de2714eb
Jason8558 <71411694+Jason8558@users.noreply.github.com>
2020-10-13 13:21:11 +1200

    login form added

--- reg_jounals/views.py
+++ reg_jounals/views.py
@@ -4,13 +4,16 @@ from .models import *
 
 
 def index(request):
-    return render(request, 'reg_jounals/index.html')
+    if request.user.is_authenticated:
+        return render(request, 'reg_jounals/index.html')
+    else: return render(request, 'reg_jounals/no_auth.html')
 def outbound_docs(request):
     if request.user.is_authenticated:
+        auth = request.user.is_authenticated
         documents = OutBoundDocument.objects.all()
         count = len(documents)
         method = str(request.method)
         usr = str(request.user.first_name)
-        return render(request, 'reg_jounals/outbound_docs.html', context={'documents':documents, 'count':count})
+        return render(request, 'reg_jounals/outbound_docs.html', context={'documents':documents, 'count':count, 'auth':auth})
     else:
         return render(request, 'reg_jounals/no_auth.html')
