375e24139461544a759c3aad81b5898c96e0ddc5
Jason8558 <71411694+Jason8558@users.noreply.github.com>
2020-10-22 17:28:10 +1200

    Update urls.py

--- reg_jounals/urls.py
+++ reg_jounals/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('outbound_docs/add', views.nr_OutBoundDocument, name='doc_outbound_add_url'),
     path('letters_of_resignation/', views.letter_of_resignation, name='resignation'),
     path('letters_of_invite/', views.letter_of_invite, name='invite'),
-    path('letters_of_invite/add', views.nr_LetterOfInvite, name='invite_add')
+    path('letters_of_invite/add', views.nr_LetterOfInvite, name='invite_add'),
+    path('letters_of_invite/<int:id>/edit', views.nr_LetterOfInvite, name='invite_add')
+
 ]
