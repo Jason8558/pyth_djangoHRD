dcc3d550246457be8ba8afcdcfe1867697d0c945
Jason8558 <bondarchyk9137@gmail.com>
2020-12-25 19:09:29 +1200

    Update settings.py

--- hrd_docFlow/settings.py
+++ hrd_docFlow/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'hrd_docflow',
         'USER': 'hrd_user',
-        'PASSWORD': 'hrd_password',
+        'PASSWORD': 'hrdpassword',
         'HOST': '172.16.23.38',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
     }
