35034895ca43f746d82e3b35d02fa9540855e8fc
Jason8558 <71411694+Jason8558@users.noreply.github.com>
2020-12-25 16:44:11 +1200

    summary

--- hrd_docFlow/settings.py
+++ hrd_docFlow/settings.py
@@ -81,7 +81,22 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'db_newsite',
+#         'USER': 'ns_user',
+#         'PASSWORD': 'ns_pass',
+#         'HOST': '172.16.20.130',   # Or an IP Address that your DB is hosted on
+#         'PORT': '3306',
+#     }
+# }
+
+
 }
+
+
 
 
 # Password validation
