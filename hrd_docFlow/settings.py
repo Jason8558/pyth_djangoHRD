1812119841b3d63d130ebb2091a87345cd5a2257
Jason8558 <71411694+Jason8558@users.noreply.github.com>
2021-01-11 09:21:46 +1200

    sumamry

--- hrd_docFlow/settings.py
+++ hrd_docFlow/settings.py
@@ -83,6 +83,7 @@ WSGI_APPLICATION = 'hrd_docFlow.wsgi.application'
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
+#         }
 
 DATABASES = {
     'default': {
