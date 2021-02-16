1c332fcba9d57552154721f13d207ba205e597eb
Jason8558 <71411694+Jason8558@users.noreply.github.com>
2021-02-16 12:08:52 +1200

    Update models.py

--- TURV/models.py
+++ TURV/models.py
@@ -6,7 +6,7 @@ class Employers(models.Model):
     position = models.ForeignKey('Position', verbose_name='Должность', db_index=True, on_delete=models.CASCADE)
     shift_personnel = models.BooleanField(verbose_name='Сменный персонал', default=False)
     stand_worktime = models.FloatField(verbose_name='Норма часов', default=0)
-    department = models.ForeignKey('Department', verbose_name='Подразделение', db_index=True, on_delete=models.CASCADE)
+    department = models.ForeignKey('Department', verbose_name='Подразделение', on_delete=models.CASCADE)
     level = models.CharField(verbose_name='Разряд/категория', max_length=256)
     positionOfPayment = models.CharField(verbose_name='Ступень оплаты', max_length=2)
 
