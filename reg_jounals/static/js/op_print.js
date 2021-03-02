cb2b4b5b1a729c87ee3596c006a74e09cbf71093
Jason8558 <>
2021-03-02 15:25:50 +1200

    sumamry

--- reg_jounals/static/js/op_print.js
+++ reg_jounals/static/js/op_print.js
@@ -4,8 +4,9 @@ var checkboxes = $('.for_print')
 var rows = $('.t_row')
 var printed_rows = 0;
 
-var WinPrint = window.open('','','left=50,top=50,width=800,height=640,toolbar=0,scrollbars=1,status=0');
-WinPrint.document.write('<table>');
+var WinPrint = window.open('','','left=50,top=50,width=1000,height=640,toolbar=0,scrollbars=1,status=0');
+WinPrint.document.write('<style> table tr {border-bottom: solid black 1px;}</style>');
+WinPrint.document.write('<table border="1" width="100%">');
 WinPrint.document.write('<thead class="thead-dark">');
 WinPrint.document.write('<tr class="tr-header">');
 WinPrint.document.write('<th></th>');
@@ -20,7 +21,7 @@ WinPrint.document.write('</thead>');
 
 for (var i = 0; i < checkboxes.length; i++) {
   if ($(checkboxes[i]).prop('checked') == true) {
-    WinPrint.document.write('<tr>' + $(rows[i]).html() + '</tr>');
+    WinPrint.document.write('<tr ">' + $(rows[i]).html() + '</tr>');
   } ;
 
 };
