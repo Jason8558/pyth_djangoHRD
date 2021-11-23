0642f7a7f900564b130da653faff0801958b4935
Jason8558 <71411694+Jason8558@users.noreply.github.com>
2021-11-23 15:57:40 +1200

    emergency_print_fix
    Срочное исправление формы

--- reg_jounals/static/TURV/js/printtabel1.js
+++ reg_jounals/static/TURV/js/printtabel1.js
@@ -5,16 +5,16 @@ function print() {
   for (var inf of info) {
     if (inf.indexOf('Chrome') >-1) {
       browser = "Chrome"
-      rcount = 46
+      rcount = 60
     }
     if (inf.indexOf('Firefox') >-1 ) {
       browser = "Firefox"
-      rcount = 40
+      rcount = 56
     }
   }
 
   var WinPrint = window.open('','','left=50,top=50,width=1500,height=1000,toolbar=0,scrollbars=1,status=0');
-  WinPrint.document.write('<link rel="stylesheet" type="text/css" href="/static/TURV/css/tabel_print2.css"> ')
+  WinPrint.document.write('<link rel="stylesheet" type="text/css" href="/static/TURV/css/tabel_print3.css"> ')
   WinPrint.document.write('<link rel="stylesheet" href="/static/TURV/css/style11.css">')
 
 
@@ -41,7 +41,7 @@ if (parts > 0) {
     for (var y = 0; y < parts-1; y++) {
 
         next = i+rcount
-        WinPrint.document.write("<table class='print_table' style='page-break-after: always;'> <thead>")
+        WinPrint.document.write("<table class='print_table' style='page-break-after: always; table-layout: fixed;'> <thead>")
         WinPrint.document.write( $(".tabel thead").html() + "</thead><tbody>")
 
         for (var i = i; i < next; i++) {
@@ -58,7 +58,7 @@ if (parts > 0) {
     i = (rows.length) - ost
 WinPrint.document.write( $(".tabel thead").html() + "</thead><tbody>")
     for (var i = i; i < rows.length; i++) {
-        
+
         WinPrint.document.write("<tr>" + rows[i].innerHTML + "</tr>" )
       }
 
@@ -70,6 +70,6 @@ else {
 }
 
 
-console.log($(".print_table")[0].html());
+
 
 }
