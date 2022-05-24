37972de6b3cb9ae4234ecfb0344986e70ebbc8bc
Jason8558 <71411694+Jason8558@users.noreply.github.com>
2022-05-25 11:31:00 +1200

    summary

--- TURV/views.py
+++ TURV/views.py
@@ -60,7 +60,7 @@ def access_check(request):
         granted = True
     return granted
 
-# Списки табелей --------------------------
+# =========== Списки табелей ==============
 
 def tabels(request):
  #Проверка на аутентификацию
@@ -467,7 +467,7 @@ def nn_tabels(request):
 
 # =========================================
 
-# Основные функции ------------------------
+# ========== Основные функции =============
 
 def tabel_create(request, id):
     if request.user.is_authenticated:
@@ -509,6 +509,7 @@ def tabel_create(request, id):
                 for i in c_items:
                     corr_items.append(i.id)
                 items = TabelItem.objects.filter(id__in=corr_items).order_by('employer')
+                # =========================================
 
 
 
@@ -646,6 +647,8 @@ def tabel_create(request, id):
     else:
         return render(request, 'reg_jounals/no_auth.html')
 
+# Добавление комментария к табелю
+
 def upd_comm(request,id):
     if request.user.is_authenticated:
 
@@ -655,6 +658,10 @@ def upd_comm(request,id):
         tabel.comm = comm
         tabel.save()
         return redirect('/turv/create/' + str(id))
+
+# ===============================
+
+# Создание нового табеля
 
 def new_tabel(request):
     if request.user.is_authenticated:
@@ -696,6 +703,10 @@ def new_tabel(request):
     else:
         return render(request, 'reg_jounals/no_auth.html')
 
+# ===============================
+
+# Создание корректирующего табеля
+
 def new_corr_tabel(request, id):
     if request.user.is_authenticated:
         tabel = Tabel.objects.get(id=id)
@@ -719,7 +730,9 @@ def new_corr_tabel(request, id):
 
             return redirect('/turv/create/' + str(last_corr.id))
 
+# ================================
 
+# ====== Действия с табелем ==================
 
 def tabel_delcheck(request,id):
     if request.user.is_authenticated:
@@ -917,6 +930,8 @@ def tabel_delitem(request, id):
         item.delete()
         return redirect(dest)
 
+# =============================================
+
 # Справочники ---------------------------------
 
 def messages_ref(request):
@@ -950,9 +965,6 @@ def new_message(request, id):
                     return redirect('/turv/messages/')
                 else:
                     return render(request, 'TURV/message-form.html', context={'form':form})
-
-
-
 
 def employers_list(request):
     if request.user.is_authenticated:
@@ -1182,15 +1194,9 @@ def total_tabels_html(request):
         return render(request, 'TURV/total.html', context={'deps':deps})
 
 
+# =========== Выгрузка ======
 
-
-
-
-
-
-# Выгрузка ------------------
-
-def unload(request):
+def old_unload(request):
     udeps = request.GET.get('udeps','')
     notulonl =  request.GET.get('nounload_only','')
     month_ = request.GET.get('uload_month','')
@@ -1218,6 +1224,10 @@ def unload(request):
                 items = TabelItem.objects.filter(employer__department_id=dep.id).filter(month=month_).filter(year=year_).filter(bound_tabel__unloaded=False).filter(bound_tabel__type__id = 1).order_by('employer')
             else:
                 items = TabelItem.objects.filter(employer__department_id=dep.id).filter(month=month_).filter(year=year_).filter(bound_tabel__type__id = 1).order_by('employer')
+
+
+
+
 
 
             if items:
@@ -1512,6 +1522,369 @@ def unload(request):
     else:
         return render(request, 'TURV/unload.html', context={"deps":deps})
 
+# ===========================
+
+# Выгрузка основных с учетом корректировок
+
+def unload(request):
+    udeps = request.GET.get('udeps','')
+    notulonl =  request.GET.get('nounload_only','')
+    month_ = request.GET.get('uload_month','')
+    year_ = request.GET.get('uload_year','')
+    if udeps:
+        udeps_l = []
+        udeps = udeps.split(',')
+        for dep in udeps:
+            udeps_l.append(dep)
+        deps = Department.objects.filter(id__in=udeps_l).order_by('id')
+    else:
+        deps = Department.objects.all().order_by('id')
+
+    if month_ and year_:
+
+        tabels = Tabel.objects.filter(department__in=deps).filter(year=year_).filter(month=month_).filter(type_id=1).filter(iscorr=0).filter(sup_check=1)
+    else:
+        tabels = []
+
+    if tabels:
+        wb = xlwt.Workbook()
+        for tabel in tabels:
+            if tabel.sup_check:
+                c_tabels = Tabel.objects.filter(iscorr=1).filter(corr_id=tabel.id).filter(sup_check=1).order_by('-id').filter(type_id=1)
+                if c_tabels:
+                    if len(c_tabels) == 1:
+                        corr_emps = []
+                        corr_items = []
+                        c_items = TabelItem.objects.filter(bound_tabel_id=c_tabels[0])
+                        for item in c_items:
+                            corr_emps.append(item.employer_id)
+                        items = TabelItem.objects.filter(~Q(employer_id__in=corr_emps)).filter(bound_tabel=tabel)
+                        for i in items:
+                            corr_items.append(i.id)
+                        for i in c_items:
+                            corr_items.append(i.id)
+                        items = TabelItem.objects.filter(id__in=corr_items).order_by('employer')
+                    else:
+                        corr_emps = []
+                        corr_items = []
+                        corr_tabels = []
+                        for c_tabel in c_tabels:
+                            corr_tabels.append(c_tabel.id)
+                        c_items = TabelItem.objects.filter(bound_tabel_id__in=corr_tabels).order_by('-bound_tabel_id')
+                        for item in c_items:
+                            c_emps = c_items.filter(employer_id=item.employer_id)
+                            if len(c_emps) == 1:
+                                corr_emps.append(item.employer_id)
+                                corr_items.append(item.id)
+                            else:
+                                last = c_emps.latest('id')
+                                corr_emps.append(last.employer_id)
+                                corr_items.append(last.id)
+                        items = TabelItem.objects.filter(~Q(employer_id__in=corr_emps)).filter(bound_tabel=tabel)
+                        for i in items:
+                            corr_items.append(i.id)
+                        items = TabelItem.objects.filter(id__in=corr_items).order_by('employer')
+
+
+
+
+
+
+
+                else:
+                    items = TabelItem.objects.filter(bound_tabel=tabel).order_by('employer')
+            else:
+                pass
+
+            if items:
+                dn = str(tabel.department.name).replace(' ', '_')
+                dn = dn.replace('-','')
+                dn = dn.replace('(','')
+                dn = dn.replace(')','')
+                dn = transliterate(dn)
+
+                ws = wb.add_sheet(dn)
+
+                i = 1
+                ws.write(0,0,'fio_vod')
+                ws.write(0,1,'dolgn')
+                ws.write(0,2,'t1')
+                ws.write(0,3,'t2')
+                ws.write(0,4,'t3')
+                ws.write(0,5,'t4')
+                ws.write(0,6,'t5')
+                ws.write(0,7,'t6')
+                ws.write(0,8,'t7')
+                ws.write(0,9,'t8')
+                ws.write(0,10,'t9')
+                ws.write(0,11,'t10')
+                ws.write(0,12,'t11')
+                ws.write(0,13,'t12')
+                ws.write(0,14,'t13')
+                ws.write(0,15,'t14')
+                ws.write(0,16,'t15')
+                ws.write(0,17,'t16')
+                ws.write(0,18,'t17')
+                ws.write(0,19,'t18')
+                ws.write(0,20,'t19')
+                ws.write(0,21,'t20')
+                ws.write(0,22,'t21')
+                ws.write(0,23,'t22')
+                ws.write(0,24,'t23')
+                ws.write(0,25,'t24')
+                ws.write(0,26,'t25')
+                ws.write(0,27,'t26')
+                ws.write(0,28,'t27')
+                ws.write(0,29,'t28')
+                ws.write(0,30,'t29')
+                ws.write(0,31,'t30')
+                ws.write(0,32,'t31')
+                ws.write(0,33,'pr1')
+                ws.write(0,34,'pr2')
+                ws.write(0,35,'pr3')
+                ws.write(0,36,'pr4')
+                ws.write(0,37,'pr5')
+                ws.write(0,38,'pr6')
+                ws.write(0,39,'pr7')
+                ws.write(0,40,'pr8')
+                ws.write(0,41,'pr9')
+                ws.write(0,42,'pr10')
+                ws.write(0,43,'pr11')
+                ws.write(0,44,'pr12')
+                ws.write(0,45,'pr13')
+                ws.write(0,46,'pr14')
+                ws.write(0,47,'pr15')
+                ws.write(0,48,'pr16')
+                ws.write(0,49,'pr17')
+                ws.write(0,50,'pr18')
+                ws.write(0,51,'pr19')
+                ws.write(0,52,'pr20')
+                ws.write(0,53,'pr21')
+                ws.write(0,54,'pr22')
+                ws.write(0,55,'pr23')
+                ws.write(0,56,'pr24')
+                ws.write(0,57,'pr25')
+                ws.write(0,58,'pr26')
+                ws.write(0,59,'pr27')
+                ws.write(0,60,'pr28')
+                ws.write(0,61,'pr29')
+                ws.write(0,62,'pr30')
+                ws.write(0,63,'pr31')
+
+                for item in items:
+                    ws.write(i,0, item.employer.fullname)
+                    ws.write(i,1, item.employer.position.name)
+
+                    if item.type_time1 == 'В':
+                        ws.write(i,2,'В')
+                    else:
+                        ws.write(i,2,item.hours1)
+
+                    if item.type_time2 == 'В':
+                        ws.write(i,3,'В')
+                    else:
+                        ws.write(i,3,item.hours2)
+
+                    if item.type_time3 == 'В':
+                        ws.write(i,4,'В')
+                    else:
+                        ws.write(i,4,item.hours3)
+
+                    if item.type_time4 == 'В':
+                        ws.write(i,5,'В')
+                    else:
+                        ws.write(i,5,item.hours4)
+
+                    if item.type_time5 == 'В':
+                        ws.write(i,6,'В')
+                    else:
+                        ws.write(i,6,item.hours5)
+
+                    if item.type_time6 == 'В':
+                        ws.write(i,7,'В')
+                    else:
+                        ws.write(i,7,item.hours6)
+
+                    if item.type_time7 == 'В':
+                        ws.write(i,8,'В')
+                    else:
+                        ws.write(i,8,item.hours7)
+
+                    if item.type_time8 == 'В':
+                        ws.write(i,9,'В')
+                    else:
+                        ws.write(i,9,item.hours8)
+
+                    if item.type_time9 == 'В':
+                        ws.write(i,10,'В')
+                    else:
+                        ws.write(i,10,item.hours9)
+
+                    if item.type_time10 == 'В':
+                        ws.write(i,11,'В')
+                    else:
+                        ws.write(i,11,item.hours10)
+
+                    if item.type_time11 == 'В':
+                        ws.write(i,12,'В')
+                    else:
+                        ws.write(i,12,item.hours11)
+
+                    if item.type_time12 == 'В':
+                        ws.write(i,13,'В')
+                    else:
+                        ws.write(i,13,item.hours12)
+
+                    if item.type_time13 == 'В':
+                        ws.write(i,14,'В')
+                    else:
+                        ws.write(i,14,item.hours13)
+
+                    if item.type_time14 == 'В':
+                        ws.write(i,15,'В')
+                    else:
+                        ws.write(i,15,item.hours14)
+
+                    if item.type_time15 == 'В':
+                        ws.write(i,16,'В')
+                    else:
+                        ws.write(i,16,item.hours15)
+
+                    if item.type_time16 == 'В':
+                        ws.write(i,17,'В')
+                    else:
+                        ws.write(i,17,item.hours16)
+
+                    if item.type_time17 == 'В':
+                        ws.write(i,18,'В')
+                    else:
+                        ws.write(i,18,item.hours17)
+
+                    if item.type_time18 == 'В':
+                        ws.write(i,19,'В')
+                    else:
+                        ws.write(i,19,item.hours18)
+
+                    if item.type_time19 == 'В':
+                        ws.write(i,20,'В')
+                    else:
+                        ws.write(i,20,item.hours19)
+
+                    if item.type_time20 == 'В':
+                        ws.write(i,21,'В')
+                    else:
+                        ws.write(i,21,item.hours20)
+
+                    if item.type_time21 == 'В':
+                        ws.write(i,22,'В')
+                    else:
+                        ws.write(i,22,item.hours21)
+
+                    if item.type_time22 == 'В':
+                        ws.write(i,23,'В')
+                    else:
+                        ws.write(i,23,item.hours22)
+
+                    if item.type_time23 == 'В':
+                        ws.write(i,24,'В')
+                    else:
+                        ws.write(i,24,item.hours23)
+
+                    if item.type_time24 == 'В':
+                        ws.write(i,25,'В')
+                    else:
+                        ws.write(i,25,item.hours24)
+
+                    if item.type_time25 == 'В':
+                        ws.write(i,26,'В')
+                    else:
+                        ws.write(i,26,item.hours25)
+
+                    if item.type_time26 == 'В':
+                        ws.write(i,27,'В')
+                    else:
+                        ws.write(i,27,item.hours26)
+
+                    if item.type_time27 == 'В':
+                        ws.write(i,28,'В')
+                    else:
+                        ws.write(i,28,item.hours27)
+
+                    if item.type_time28 == 'В':
+                        ws.write(i,29,'В')
+                    else:
+                        ws.write(i,29,item.hours28)
+
+                    if item.type_time29 == 'В':
+                        ws.write(i,30,'В')
+                    else:
+                        ws.write(i,30,item.hours29)
+
+                    if item.type_time30 == 'В':
+                        ws.write(i,31,'В')
+                    else:
+                        ws.write(i,31,item.hours30)
+
+                    if item.type_time31 == 'В':
+                        ws.write(i,32,'В')
+                    else:
+                        ws.write(i,32,item.hours31)
+                    ws.write(i,33,item.type_time1)
+                    ws.write(i,34,item.type_time2)
+                    ws.write(i,35,item.type_time3)
+                    ws.write(i,36,item.type_time4)
+                    ws.write(i,37,item.type_time5)
+                    ws.write(i,38,item.type_time6)
+                    ws.write(i,39,item.type_time7)
+                    ws.write(i,40,item.type_time8)
+                    ws.write(i,41,item.type_time9)
+                    ws.write(i,42,item.type_time10)
+                    ws.write(i,43,item.type_time11)
+                    ws.write(i,44,item.type_time12)
+                    ws.write(i,45,item.type_time13)
+                    ws.write(i,46,item.type_time14)
+                    ws.write(i,47,item.type_time15)
+                    ws.write(i,48,item.type_time16)
+                    ws.write(i,49,item.type_time17)
+                    ws.write(i,50,item.type_time18)
+                    ws.write(i,51,item.type_time19)
+                    ws.write(i,52,item.type_time20)
+                    ws.write(i,53,item.type_time21)
+                    ws.write(i,54,item.type_time22)
+                    ws.write(i,55,item.type_time23)
+                    ws.write(i,56,item.type_time24)
+                    ws.write(i,57,item.type_time25)
+                    ws.write(i,58,item.type_time26)
+                    ws.write(i,59,item.type_time27)
+                    ws.write(i,60,item.type_time28)
+                    ws.write(i,61,item.type_time29)
+                    ws.write(i,62,item.type_time30)
+                    ws.write(i,63,item.type_time31)
+                    i = i+1
+            else:
+                pass
+        name = str(month_)+'_'+str(year_)+'.xls'
+        wb.save(name)
+
+        fp = open(name, "rb")
+        response = HttpResponse(fp.read())
+        fp.close();
+
+        file_type = 'application/octet-stream'
+        response['Content-Type'] = file_type
+        response['Content-Length'] = str(os.stat(name).st_size)
+        response['Content-Disposition'] = "attachment; filename=%s" %name
+
+        return response;
+
+    else:
+        message = 'Табелей для выгрузки нет!'
+        return render(request, 'TURV/unload.html', context={"deps":deps, 'message':message})
+
+# ==========================
+
+# Выгрузка вредности
+
 def toxic_unload(request):
     if request.user.is_authenticated:
         month = request.GET.get('month', '')
@@ -1585,6 +1958,10 @@ def toxic_unload(request):
         else:
             return render(request, 'TURV/toxic-unload.html')
 
+# ===========================
+
+# Выгрузка совмещения
+
 def unite_unload(request):
     if request.user.is_authenticated:
         month = request.GET.get('month', '')
@@ -1647,7 +2024,11 @@ def unite_unload(request):
         else:
             return render(request, 'TURV/unite-unload.html')
 
+# =========================
+
 # ---------------------------
+
+# ===== Работа с нормой =======
 
 def upd_norma(request):
     if request.user.is_authenticated:
@@ -1676,3 +2057,5 @@ def new_norma(request):
 
 
         return JsonResponse(message, safe=False)
+
+# ==============================
