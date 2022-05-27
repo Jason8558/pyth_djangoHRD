72b11d73dfcd94f8243c0f36cb72bb7ce9e56dad
Jason8558 <71411694+Jason8558@users.noreply.github.com>
2022-05-27 15:13:15 +1200

    Update views.py

--- TURV/views.py
+++ TURV/views.py
@@ -1952,7 +1952,7 @@ def toxic_unload(request):
                         current_tabel.unloaded = True
                         current_tabel.save()
                         # name =  'S:\Сетевые папки\Обмен\Бухгалтерия\РАСЧЕТНЫЙ ОТДЕЛ\ВыгрузкаВредности' + sep + str(dn) + '_' + str(month)+'_'+str(year)+ '_vrednost.xls'
-                        name =  '/mnt/1c-u-HRD_Uploads/ВыгрузкаВредности/' + str(dn) + '_' + str(month)+'_'+str(year)+ '_vrednost.xlsx'
+                        name =  '/mnt/1c-u-HRD_Uploads/ВыгрузкаВредности/' + str(dn) + '_' + str(month)+'_'+str(year)+ '_vrednost.xls'
 
                         wb.save(name)
 
