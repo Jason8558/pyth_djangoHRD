d25e2c153597386d154826b508e971363d6e74c0
Jason8558 <bondarchyk9137@gmail.com>
2022-10-24 16:26:06 +1200

    Update views.py

--- vac_shed/views.py
+++ vac_shed/views.py
@@ -63,8 +63,8 @@ def vacshed_global_json(request, year, dep, per):
             items = items_main.union(items_move)
         else:
             if dep != 0:
-                items = VacantionSheduleItem.objects.filter(bound_shed__year=year).filter(bound_shed__dep = dep).values('id', 'bound_shed__dep__name' , 'emp__aup__name' ,'emp', 'emp__fullname', 'dur_from', 'dur_to', 'days_count', 'move_from', 'move_to', 'child_year', 'days_count_move', 'city', 'emp__position__name').order_by('bound_shed__dep__name', 'emp', 'id', 'dur_from')
-                items_aup = VacantionSheduleItem.objects.filter(bound_shed__year=year).filter(emp__aup_id = dep).values('id', 'bound_shed__dep__name' , 'emp__aup__name' , 'emp', 'emp__fullname', 'dur_from', 'dur_to', 'days_count', 'move_from', 'move_to', 'child_year', 'days_count_move', 'city', 'emp__position__name').order_by('bound_shed__dep__name', 'emp', 'id', 'dur_from')
+                items = VacantionSheduleItem.objects.filter(bound_shed__year=year).filter(bound_shed__dep = dep).values('id', 'bound_shed__dep__name' , 'emp__aup__name' ,'emp', 'emp__fullname', 'dur_from', 'dur_to', 'days_count', 'move_from', 'move_to', 'child_year', 'days_count_move', 'city', 'emp__position__name').order_by('bound_shed__dep__name', 'emp__fullname', 'id', 'dur_from')
+                items_aup = VacantionSheduleItem.objects.filter(bound_shed__year=year).filter(emp__aup_id = dep).values('id', 'bound_shed__dep__name' , 'emp__aup__name' , 'emp', 'emp__fullname', 'dur_from', 'dur_to', 'days_count', 'move_from', 'move_to', 'child_year', 'days_count_move', 'city', 'emp__position__name').order_by('bound_shed__dep__name', 'emp__fullname', 'id', 'dur_from')
                 items = items.union(items_aup)
             else:
                 if per != 0:
