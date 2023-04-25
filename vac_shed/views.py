b26700ce70d0bbceee2eb6d7642b7ad8b114af88
Jason8558 <bondarchyk9137@gmail.com>
2023-04-26 10:53:11 +1200

    Revert "Update views.py"
    This reverts commit 5a69f606effbbeb5cd929f401c317a55e0483092.

--- vac_shed/views.py
+++ vac_shed/views.py
@@ -199,12 +199,7 @@ def vacshed_emp_info(request, year, emp):
 
 def vacshed_create(request,vs):
     if request.user.is_authenticated:
-        granted = 0
-        for group in request.user.groups.all():
-            if group.name == "Руководитель СУП":
-                granted = 1
-        if request.user.is_superuser:
-            granted = 1
+        granted = ugroup(request)
         vacshed = VacantionShedule.objects.get(id=vs)
 
         return render(request, 'vac_shed/vs-create.html', context={'vacshed':vacshed, 'granted':granted})
