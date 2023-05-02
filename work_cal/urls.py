4bb79982edeea0c4e4573b4691d70c7e89a1a197
Jason8558 <bondarchyk9137@gmail.com>
2023-05-02 16:01:38 +1200

    Update urls.py

--- work_cal/urls.py
+++ work_cal/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import WorkCalAPI
+
 urlpatterns = [
 path('<str:year>', views.get_cal, name='wc-get'),
 path('<str:year>/html/', views.get_cal_html, name='wc-get-html'),
@@ -9,7 +9,7 @@ path('<str:year>/html/clear', views.clear, name='wc-clear'),
 path('<str:year>/<str:month>', views.get_month, name='wc-month'),
 path('write/', views.new_record, name='wc-newrecord'),
 path('', views.calendar_list, name='wc-list'),
-path('api/v1/getcal', WorkCalAPI.as_view())
+# path('api/v1/getcal', WorkCalAPI.as_view())
 
 
 ]
