b5369e95e79aebdb8914008e788915c23bc783fc
Jason8558 <bondarchyk9137@gmail.com>
2023-05-02 15:59:18 +1200

    summary

--- work_cal/models.py
+++ work_cal/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class WorkCalendarRecord(models.Model):
     year = models.CharField(max_length=4, verbose_name='Год', blank=False, null=False)
     month = models.IntegerField(verbose_name='Месяц', blank=False, null=False)
-    days = models.CharField(verbose_name='Дни', max_length=256, blank=False, null=False)
+    days = models.CharField(verbose_name='Дни', max_length=256, blank=True, null=False)
 
     class Meta:
         verbose_name = 'Запись в календаре'
--- work_cal/urls.py
+++ work_cal/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from . import views
+from .views import WorkCalAPI
 urlpatterns = [
 path('<str:year>', views.get_cal, name='wc-get'),
-path('<str:year>/<str:month>', views.get_month, name='wc-month')
+path('<str:year>/html/', views.get_cal_html, name='wc-get-html'),
+path('<str:year>/html/upd', views.upd_record, name='wc-upd'),
+path('<str:year>/html/clear', views.clear, name='wc-clear'),
+path('<str:year>/<str:month>', views.get_month, name='wc-month'),
+path('write/', views.new_record, name='wc-newrecord'),
+path('', views.calendar_list, name='wc-list'),
+path('api/v1/getcal', WorkCalAPI.as_view())
+
 
 ]
