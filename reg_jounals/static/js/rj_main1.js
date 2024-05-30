e34e121b1b0e8525b56dc1de9ec6cf690b3f5e34
Jason8558 <bondarchyk9137@gmail.com>
2024-05-30 16:47:56 +1200

    summary

--- reg_jounals/static/js/rj_main1.js
+++ reg_jounals/static/js/rj_main1.js
@@ -679,8 +679,52 @@ function get_employers_from_department(department_id, employers_field, employer_
 
   });
 
+
+
   // if (employer_id != 0) {
   //   document.getElementById('order-of-personell-bound-employer-field').value = employer_id
   // }
+
+}
+
+function open_additional_menu(el, additional_menu_panel) {
+  // открыть доп меню
+  close_all_panels()
+
+  additional_menu_panel = document.getElementById(additional_menu_panel)
+
+  coords = el.getBoundingClientRect()
+
+  additional_menu_panel.style.display   = 'flex'
+  additional_menu_panel.style.position  = 'absolute'
+  additional_menu_panel.style.top       = (Number(coords.top) + 40) + 'px'
+  
+  additional_menu_panel.style.height    = 'max-content'
+  if (additional_menu_panel.classList.contains('search')) {
+
+  }
+  else {
+      additional_menu_panel.style.width    = 'max-content'
+      additional_menu_panel.style.left      = coords.left + 'px'
+  }
+   
+
+  additional_menu_panel.addEventListener('mouseleave', (vsp) => {
+    close_all_panels()
+  })
+
+  el.onclick = function() {
+    
+    if (additional_menu_panel.style.display == 'none') {
+      additional_menu_panel.style.display = 'flex'}
+    else {
+      additional_menu_panel.style.display = 'none'
+      
+    }
+
+  }
+ 
+
+ //el.onclick = 'open_additional_menu(this)'
 
 }
