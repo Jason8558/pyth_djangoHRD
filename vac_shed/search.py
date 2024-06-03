88e5dc1434917a50962eb3d3c8e7ccb8f42f5e5f
Jason8558 <bondarchyk9137@gmail.com>
2024-06-03 14:31:29 +1200

    summary

--- vac_shed/search.py
+++ vac_shed/search.py
@@ -17,3 +17,16 @@ def main_search(fields):
         search_result = []
     
     return(search_result)
+
+def vacshed_global(request):
+    search_query = {
+        'year':        request.GET.get('vs-glob-year',     DT.datetime.now(year)),
+        'department':  request.GET.get('vs-glob-dep',      ''),
+        'position':    request.GET.get('vs-glob-pos',      ''),
+        'filial':      request.GET.get('vs-glob-fil',      ''),
+        'territory':   request.GET.get('vs-glob-terr',     ''),
+        'from':        request.GET.get('vs-glob-per',      ''),
+        'employers':   request.GET.get('vs-glob-emp-id',   '')
+    }
+
+    return ''
