ff8f8b2c96e0c17f5f1dda96e97c6a0095f8d197
Jason8558 <bondarchyk9137@gmail.com>
2025-01-16 13:35:26 +1200

    Update manage.py

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hrd_docFlow.settings_local_realDB')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hrd_docFlow.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
